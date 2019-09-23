dc0f385f15925ddb76849985743c934653db50f0
Sirikonss <sirikon.exo@gmail.com>
2019-09-23 23:16:52 +0700

    edit readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# django-polls
-## Author
-Sirikon Songsaengthong 6110545660
+## Django Polls Application
+ (describe the application)
+
+ ### Author
+ Sirikon Songsaengthong 6110545660
+
+ ## Requirements
+
+ The application requires
+ * Python 3.6 or newer
+ * Django 2.1.2 or newer
+ * Python add-on modules as in [requirements.txt](requirements.txt)
+
+ ## Running
+ - git clone https://github.com/Sirikonss/django-polls.git
+ - Make sure you already meet all requirements.
+ - "python3 manage.py runserver"
+
+ 
+
--- polls/models.py
+++ polls/models.py
@@ -16,22 +16,7 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-    def test_was_published_recently_with_old_question(self):
-        """was_published_recently() returns False for questions whose pub_date
-        is older than 1 day.
-        """
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date=time)
-        self.assertIs(old_question.was_published_recently(), False)
-
-def test_was_published_recently_with_recent_question(self):
-        """
-        was_published_recently() returns True for questions whose pub_date
-        is within the last day.
-        """
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-        recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+    
 
 
 class Choice(models.Model):
@@ -41,3 +26,11 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+    def vote_count(id):
+       """Return total votes for a given poll. id is poll id"""
+       # Hint: get the question from the database 
+       #       Easiest is use Question.objects.get(...) 
+       # question has a choice_set attribute that is a set of choices
+       # for that question.
+       
